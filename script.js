5f6c9117c3105f5eb4e3a5d5a525637589d353c5
Vívian Vaz <Vivianvaz5125@gmail.com>
2024-10-21 16:29:52 -0300

    up

---
+++ script.js
@@ -0,0 +1,8 @@
+alert('Bem vindo ao consultório! Esta é a fila de espera:')
+let queue = ['1ºLuis', '2ºAntônia', '3ºjosé' ]
+alert('1ºLuis, 2ºAntônia, 3ºjosé, esta é a fila de espera')
+alert('Um novo paciente entrou na fila, o nome dele é Mátias então ele será o ultimo')
+queue.push('Mátias')
+alert(`${queue}`)
+queue.shift('1ºLuis')
+alert('O primeiro paciente da fila de espera foi consultado!')
